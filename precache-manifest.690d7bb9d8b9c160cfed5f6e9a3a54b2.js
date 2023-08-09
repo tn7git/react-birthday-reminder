e56abce61f1d240a32e29279ca822400c6bf74d9
tn7git <90912178+tn7git@users.noreply.github.com>
2023-08-09 21:34:07 +1000

    Updates

---
+++ precache-manifest.690d7bb9d8b9c160cfed5f6e9a3a54b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e2f755ff0739ec50ad687640ff7219",
+    "url": "/react-birthday-reminder/index.html"
+  },
+  {
+    "revision": "e7fd7beca074d080933d",
+    "url": "/react-birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fd7beca074d080933d",
+    "url": "/react-birthday-reminder/static/js/main.6d55631f.chunk.js"
+  },
+  {
+    "revision": "ca18d9339a309e58c265",
+    "url": "/react-birthday-reminder/static/js/runtime-main.4fe2f5be.js"
+  }
+]);
